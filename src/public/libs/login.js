02f3d55b7c5d70041b44f1e5a812c805d2138e33
technical_longtail <technical@longtail.com.au>
2016-02-26 17:29:10 +0800

    Login page

---
+++ src/public/libs/login.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $("#LoginButton").click(function(){
+    var username = $("#username").val();
+    var password = $("#password").val();
+    $.ajax({
+      type: "POST",
+      url: "/auth/login",
+      data: JSON.stringify({ username: username, password: password }),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data){
+        alert(JSON.stringify(data));
+      },
+      failure: function(errMsg) {
+          alert(errMsg);
+      }
+    });
+  });
+
+   $("#FacebookButton").click(function(){
+    $.ajax({
+      type: "GET",
+      url: "/auth/facebook",
+      crossDomain: true,
+      contentType: "application/json; charset=utf-8",
+      dataType: "jsonp",
+      success: function(data){
+        alert(JSON.stringify(data));
+      },
+      failure: function(errMsg) {
+          alert(errMsg);
+      }
+    });
+  });
+});
